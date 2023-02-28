3e047dc5d675cd28a895c6caaf3b2029d876e578
Yorald <113071186+Yorald@users.noreply.github.com>
2023-02-28 13:07:56 +0100

    Update cwiczenie_palindrom.py

--- cwiczenie_palindrom.py
+++ cwiczenie_palindrom.py
@@ -1,15 +1,9 @@
 def palindrome_check(word):
-    word_backwards = []
-    for i in word[::-1]:
-        word_backwards.append(i)
-    print(word_backwards)
-    word_backwards_string = "".join(word_backwards)
-    print(word_backwards_string)
-    if word_backwards_string == word:
-        print("To słowo jest palindromem!")
+    if word == word[::-1]:
+        True, print("Słowo jest palindromem")
     else:
-        print("To słowo NIE jest palindromem!")    
-
+        False, print("Słowo NIE jest palindromem")
+        
 palindrome_check("kajak")
     
 
