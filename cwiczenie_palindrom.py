3a04e94d2c043e0b10873f23c7949b2acf36d35a
Yorald <maksym.miazek@gmail.com>
2023-02-28 11:22:12 +0100

    Add cwiczenie_palindrom.py

---
+++ cwiczenie_palindrom.py
@@ -0,0 +1,15 @@
+def palindrome_check(word):
+    word_backwards = []
+    for i in word[::-1]:
+        word_backwards.append(i)
+    print(word_backwards)
+    word_backwards_string = "".join(word_backwards)
+    print(word_backwards_string)
+    if word_backwards_string == word:
+        print("To słowo jest palindromem!!git")
+    else:
+        print("To słowo NIE jest palindromem!")    
+
+palindrome_check("kajak")
+    
+
